bb4a873e25330334447bc4208f3f1f8321ff48d9
Sagar Singh <arcsagar.91@gmail.com>
2026-07-06 13:21:36 +0530

    Add: dota2-senate - submission-2

---
+++ Data Structures & Algorithms/dota2-senate/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        R_queue = deque()
+        D_queue = deque()
+
+        for i in range(len(senate)):
+            if senate[i] == 'R':
+                R_queue.append(i)
+            else:
+                D_queue.append(i)
+
+        while R_queue and D_queue:
+            rad, dire = R_queue.popleft(), D_queue.popleft()
+
+            if rad < dire:
+                R_queue.append(rad + len(senate))
+            else:
+                D_queue.append(dire + len(senate))
+        return "Radiant" if R_queue else "Dire" 
