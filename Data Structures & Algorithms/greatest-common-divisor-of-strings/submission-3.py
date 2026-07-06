9c2dade4599a482962dd9f9924e76f2c567cab42
Sagar Singh <arcsagar.91@gmail.com>
2026-07-07 01:45:52 +0530

    Add: greatest-common-divisor-of-strings - submission-3

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+
+        limitor = str1 if len(str1) <= len(str2) else str2
+        strentor = str1 if len(str1) > len(str2) else str2
+        def valid_divisor(string1, string2):
+            
+            l = 0 
+            for r in range(len(string1)-1, -1, -1):
+                if string1[l:r+1] * (len(string1)//(r+1-l)) == string1 and string1[l:r+1] * (len(string2)//(r+1-l)) == string2:
+                    return string1[l:r+1]
+            return ""
+        
+        return valid_divisor(limitor, strentor)
+
+
