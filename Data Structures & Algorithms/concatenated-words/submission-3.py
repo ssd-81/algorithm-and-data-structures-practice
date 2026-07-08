45f4f0a482d94772ef23e70d5b04cda0a866b1af
Sagar Singh <arcsagar.91@gmail.com>
2026-07-08 11:52:19 +0530

    Add: concatenated-words - submission-3

---
+++ Data Structures & Algorithms/concatenated-words/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        # creating a hash map storing words for constant access
+        hash_map = Counter(words)
+
+        res = []
+        def dfs(word):
+            for i in range(1, len(word)-1):
+                first = word[:i] 
+                second = word[i:]
+                if first in hash_map and second in hash_map:
+                    return True 
+                if first in hash_map and dfs(second):
+                    return True 
+            return False 
+        
+        for word in words:
+            if dfs(word):
+                res.append(word)
+        return res
