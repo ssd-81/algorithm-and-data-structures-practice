6fea45d204b347570ad35b6ea8f555d35e34e645
Sagar Singh <arcsagar.91@gmail.com>
2026-07-10 13:45:33 +0530

    Add: max-area-of-island - submission-14

---
+++ Data Structures & Algorithms/max-area-of-island/submission-14.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
+        def in_bounds(r, c):
+            return 0 <= r < len(grid) and 0 <= c < len(grid[0])
+
+        visited = set()
+        max_area = 0 
+        dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        def dfs(r, c):
+            visited.add((r, c))
+            for dr, dc in dirs:
+                nc, nr = r + dr, c + dc
+
+                if in_bounds(nc, nr) and (nc, nr) not in visited and grid[nc][nr] == 1 :
+                    return 1 + dfs(nc, nr)
+            return 1
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if (r,c) not in visited and grid[r][c] == 1:
+                    max_area = max(max_area, dfs(r,c))
+        return max_area
