10f1a6f93b72527643bd4b8c29041d6104af2691
Sagar Singh <arcsagar.91@gmail.com>
2026-07-16 14:06:20 +0530

    Add: zigzag-conversion - submission-5

---
+++ Data Structures & Algorithms/zigzag-conversion/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        res = ""
+        inc = 2 * (numRows-1)
+
+        for r in range(numRows):
+            for j in range(r, len(s), inc):
+                res += s[j]
+
+                if r > 0 and r < (numRows-1) and j + inc - 2 * r < len(s):
+                    res += s[j + inc - 2 * r]
+        
+        return res 
