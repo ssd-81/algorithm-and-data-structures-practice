f8aeabaed74865cd6998bdfaa9379df5bc1487c7
Sagar Singh <arcsagar.91@gmail.com>
2026-07-16 15:37:38 +0530

    Add: design-word-search-data-structure - submission-11

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-11.py
@@ -0,0 +1,38 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False 
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()    
+
+    def addWord(self, word: str) -> None:
+        node = self.root 
+
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.isWord = True 
+
+    def search(self, word: str) -> bool:
+        
+        def search_in(node, idx):
+            if idx == len(word):
+                return False
+            
+            for i in range(idx, len(word)):
+                if word[i] == '.':
+                    for n in node.children:
+                        if(search_in(node.children[n], idx+1)):
+                            return True 
+                else:
+                    if word[i] not in node.children:
+                        return False
+                    node = node.children[word[i]] 
+            return node.isWord
+        return search_in(self.root, 0)
+
+
+            
