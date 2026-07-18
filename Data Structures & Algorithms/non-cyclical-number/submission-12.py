0b4e0e0546705f09a2a91c9adcd42a2817253165
Sagar Singh <arcsagar.91@gmail.com>
2026-07-19 04:48:25 +0530

    Add: non-cyclical-number - submission-12

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-12.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        leader, trailer = self.sumOfSquares(n), self.sumOfSquares(self.sumOfSquares(n))
+        print(leader, trailer)
+        while leader != trailer:
+            if leader == 1:
+                return True
+            leader = self.sumOfSquares(self.sumOfSquares(leader))
+            trailer = self.sumOfSquares(trailer)
+            print(leader, trailer)
+        return leader == 1
+
+    def sumOfSquares(self, n):
+        total = 0 
+
+        while n:
+            total += (n%10)**2
+            n = n // 10 
+        return total 
