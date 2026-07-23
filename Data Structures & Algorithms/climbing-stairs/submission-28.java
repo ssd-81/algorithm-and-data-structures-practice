aa595527a34e2090c63215140fb0e51969b0ab9d
Sagar Singh <arcsagar.91@gmail.com>
2026-07-23 17:02:05 +0530

    Add: climbing-stairs - submission-28

---
+++ Data Structures & Algorithms/climbing-stairs/submission-28.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        int[] arr = new int[n];
+
+        arr[n-1] = 1;
+        arr[n-2] = 1;
+
+        for(int i = n-3; i >= 0; i--){
+            arr[i] = arr[i+1] + arr[i+2];
+        }
+
+        return arr[0] + arr[1];
+    }
+}
