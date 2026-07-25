17d40487e1a9049808a75b5b7f4b9a0148070193
Sagar Singh <arcsagar.91@gmail.com>
2026-07-25 18:12:49 +0530

    Add: contains-duplicate-ii - submission-7

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashSet = set()
+        L = 0 
+
+        for R in range(len(nums)):
+            if (R-L > k):
+                hashSet.remove(nums[L])
+                L += 1
+            if nums[R] in hashSet:
+                return True 
+            
+            hashSet.add(nums[R])
+        return False 
