17e3e0e3e2b3ad50eafd13ef2e3a3bb787d62704
Sagar Singh <arcsagar.91@gmail.com>
2026-07-26 23:34:36 +0530

    Add: house-robber - submission-29

---
+++ Data Structures & Algorithms/house-robber/submission-29.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        choices = [nums[0], max(nums[0], nums[1])]
+
+        for i in range(2, len(nums)):
+            temp = choices[1]
+            choices[1] = max(nums[i] + choices[0], choices[1])
+            choices[0] = temp 
+        return choices[-1]
